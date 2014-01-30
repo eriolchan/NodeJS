678e21a65566c5dcaa5aeef38a7ba0fa281ad08c
Eric Chan <eriolchan@gmail.com>
2014-01-30 23:47:57 +0800

    checkin hour04 example

--- 24hours/hour01/example01/server.js
+++ 24hours/hour01/example01/server.js
@@ -1,5 +1,5 @@
 var http = require('http');
-http.createServer(function (req, res) {
+http.createServer(function(req, res) {
     res. writeHead(200, {'Context-Type': 'text/plain'});
     res.end('I am programming in Node.js\n');
 }).listen(10086, "127.0.0.1");
