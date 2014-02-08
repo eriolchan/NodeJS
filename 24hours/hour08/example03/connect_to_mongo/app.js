0cf1230bb08a2a8e059cd617a038142058aea1cc
Eric Chan <eriolchan@gmail.com>
2014-02-08 11:31:13 +0800

    update example03 for hour08

---
+++ 24hours/hour08/example03/connect_to_mongo/app.js
@@ -0,0 +1,102 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+var mongoose = require('mongoose');
+
+var app = express();
+
+mongoose.connect('mongodb://localhost/todo_development');
+
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var Task = new Schema({
+    task: String
+});
+
+var Task = mongoose.model('Task', Task);
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+
+app.get('/tasks', function(req, res) {
+    Task.find({}, function(err, docs) {
+        res.render('tasks/index', {
+            title: 'Todos index view',
+            docs: docs
+        });
+    });
+});
+
+app.get('/tasks/new', function(req, res) {
+    res.render('tasks/new', {
+        title: 'New Task'
+    });
+});
+
+app.post('/tasks', function(req, res) {
+    var task = new Task(req.body.task);
+    task.save(function(err) {
+        if (!err) {
+            res.redirect('/tasks');
+        } else {
+            res.redirect('/tasks/new');
+        }
+    });
+});
+
+app.get('/tasks/:id/edit', function(req, res) {
+    Task.findById(req.params.id, function(err, doc) {
+        res.render('tasks/edit', {
+            title: 'Edit Task View',
+            task: doc
+        });
+    });
+});
+
+app.put('/tasks/:id', function(req, res) {
+    Task.findById(req.params.id, function(err, doc) {
+        doc.task = req.body.task.task;
+        doc.save(function(err) {
+            if (!err) {
+                res.redirect('/tasks');
+            } else {
+                console.log(err);
+            }
+        });
+    });
+});
+
+app.del('/tasks/:id', function(req, res) {
+    Task.findOne({ _id: req.params.id }, function(err, doc) {
+        doc.remove(function() {
+            res.redirect('/tasks');
+        });
+    });
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
