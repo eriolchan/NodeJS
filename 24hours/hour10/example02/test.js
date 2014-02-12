a8bcb69d3ac5a4f1c3af05596caeda78f689cf3d
Eric Chan <eriolchan@gmail.com>
2014-02-12 13:19:13 +0800

    example for hour10

---
+++ 24hours/hour10/example02/test.js
@@ -0,0 +1,11 @@
+exports.firstTest = function(test) {
+    test.expect(1);
+    test.strictEqual("hello", "hello");
+    test.done();
+};
+
+exports.secondTest = function(test) {
+    test.expect(1);
+    test.strictEqual("hello", "world");
+    test.done();
+};
